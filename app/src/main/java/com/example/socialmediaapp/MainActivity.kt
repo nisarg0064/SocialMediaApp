bddbb699e430f8e3abd326726ca93883a56397d6
nisarg.rajput <nisarg.rajput@WM-DEV-03>
2023-04-14 09:18:09 +0530

    second

--- app/src/main/java/com/example/socialmediaapp/MainActivity.kt
+++ app/src/main/java/com/example/socialmediaapp/MainActivity.kt
@@ -8,6 +8,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val intent: Intent
+        val intent: Intent =Intent(this,this::class.java)
     }
 }
